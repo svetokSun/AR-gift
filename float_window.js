078978bf0cc0c543d814bd84531468693229bc25
svetokSun <79097116+svetokSun@users.noreply.github.com>
2021-12-28 18:32:13 +0300

    Update float_window.js

--- float_window.js
+++ float_window.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main_page.css';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
-
-
 <div>
  <h1>hello!</h1>
 </div>

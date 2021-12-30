590a601e2c6b27507d5ced009a0fcd036da9c77a
svetokSun <79097116+svetokSun@users.noreply.github.com>
2021-12-30 19:04:41 +0300

    Update float-window.js

--- float-window.js
+++ float-window.js
@@ -3,10 +3,6 @@ button.addEventListener('click', function () {
     $('.second-block').show()
 });
 
-let button = document.querySelector('.button-next');
-button.addEventListener('click', function () {
-    $('.float-block').fadeOut(20)
-});
 let background = document.querySelector('.button-finish');
 background.addEventListener('click', function () {
     $('.second-block').fadeOut(20)

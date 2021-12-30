febd3d6abb2a1246042b222b30c731690ee25db5
svetokSun <79097116+svetokSun@users.noreply.github.com>
2021-12-30 18:19:42 +0300

    Update and rename float_window.js to float-window.js

---
+++ float-window.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('.button-next');
+button.addEventListener('click', function () {
+    $('.second-block').show()
+});
+let background = document.querySelector('.button-finish');
+background.addEventListener('click', function () {
+    $('.second-block').fadeOut(20)
+})

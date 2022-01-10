4a7ebf00860f1040d213bfa35a6814f45258283b
svetokSun <svetok1106@mail.ru>
2022-01-11 01:14:06 +0500

    a href without function

--- float-window.js
+++ float-window.js
@@ -3,8 +3,3 @@ button.addEventListener('click', function () {
     $('.second-block').show()
     $('.float-block').fadeOut(20)
 });
-
-let background = document.querySelector('.button-finish');
-background.addEventListener('click', function () {
-    $('.second-block').onclick="https://svetoksun.github.io/AR-page/"
-})

37e7477726e8dee9b410ae9f8d15fb6e6c6d8b7c
svetokSun <79097116+svetokSun@users.noreply.github.com>
2022-01-22 19:27:45 +0500

    corrections

--- float-window.js
+++ float-window.js
@@ -3,3 +3,9 @@ button.addEventListener('click', function () {
     $('.second-block').show()
     $('.float-block').fadeOut(20)
 });
+
+let button1 = document.querySelector('.button-medium');
+button1.addEventListener('click', function () {
+    $('.second-block').fadeOut(20)
+    $('.third-block').show()
+})

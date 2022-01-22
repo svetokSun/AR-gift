f25109ca25fc43410b65022b2a81d1d9fdedbba1
svetokSun <79097116+svetokSun@users.noreply.github.com>
2022-01-22 19:31:59 +0500

    1 2

--- float-window.js
+++ float-window.js
@@ -1,11 +1,11 @@
-let button = document.querySelector('.button-next');
-button.addEventListener('click', function () {
+let button1 = document.querySelector('.button-next');
+button1.addEventListener('click', function () {
     $('.second-block').show()
     $('.float-block').fadeOut(20)
 });
 
-let button1 = document.querySelector('.button-medium');
-button1.addEventListener('click', function () {
+let button2 = document.querySelector('.button-medium');
+button2.addEventListener('click', function () {
     $('.second-block').fadeOut(20)
     $('.third-block').show()
 })
